c684f6ff600396f5f0f79ca19131b4cfc14087af
Vicky Thakkar <vickymthakkar@gmail.com>
2023-09-29 00:24:31 +0530

    changes

--- src/app/dashboard/dashboard/account/account.component.ts
+++ src/app/dashboard/dashboard/account/account.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { CrudService } from 'src/app/services/crud.service';
+import { NgxSpinnerService } from "ngx-spinner";
+import { CommonService } from 'src/app/services/common.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-account',
@@ -6,5 +10,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./account.component.scss']
 })
 export class AccountComponent {
+  public angadiya: any = [];
+  public client: any = [];
+  public others: any = [];
+  constructor(private crudService: CrudService, private spinner: NgxSpinnerService, private commonService: CommonService, private router: Router) {
 
+  }
+
+  editAccount(account: any) {
+    console.log(account)
+    this.router.navigate([`/dashboard/account/edit`], {state: account});
+  }
+  back() {
+    this.router.navigate(['/dashboard/main']);
+  }
+  ngOnInit() {
+    this.spinner.show();
+    const user = JSON.parse(sessionStorage.getItem('userDetails')!);
+    this.crudService.postByUrl('/AccountList', {
+      DeviceId: "5c1e2fcc27ce7a8e",
+      Token: user.Token,
+      LoginId: user.Guid
+    }).subscribe({
+      next: (res: any) => {
+        this.spinner.hide();
+        console.log("res", res);
+        this.angadiya = res.filter((r: any) => r.Type.toLowerCase() === 'angadiya');
+        this.client = res.filter((r: any) => r.Type.toLowerCase() === 'client');
+        this.others = res.filter((r: any) => r.Type.toLowerCase() !== 'angadiya' && r.Type.toLowerCase() !== 'client');
+      },
+      error: (err) => {
+        console.log("err");
+        this.spinner.hide();
+      }
+    })
+  }
 }
