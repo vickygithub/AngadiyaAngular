c345c659ccb2f46b3fcc817b23754c70de1ea7c0
Vicky Thakkar <vickymthakkar@gmail.com>
2023-10-07 01:01:09 +0530

    change

--- src/app/limit-mobile-number.directive.ts
+++ src/app/limit-mobile-number.directive.ts
@@ -1,15 +1,18 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Optional } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 @Directive({
   selector: '[appLimitMobileNumber]'
 })
 export class LimitMobileNumberDirective {
 
-  constructor(private el: ElementRef) { }
+  constructor(private el: ElementRef, @Optional() private ngModel: NgModel) { }
   @HostListener('keyup', ['$event.target.value'])
   onKeyUp(value: string): void {
-    if (value.length > 10) {
-      this.el.nativeElement.value = value.slice(0, 10);
+    if (String(value).length > 10) {
+      this.el.nativeElement.value = String(value).slice(0, 10);
+      // Update ngModel with the truncated value
+      this.ngModel?.update.emit(value.slice(0, 10));
     }
   }
 }
