f796e0fb852a774a15401631b423c19cf5261194
Vicky Thakkar <vickymthakkar@gmail.com>
2023-11-15 15:43:48 +0530

    Revert "changes"
    This reverts commit 23407ad7f14c5f44934ffe45cc8d0ebd18ee33a0.

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -3,7 +3,6 @@ import { AuthService } from '../app/auth.service';
 import { inject } from '@angular/core';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  const loggedInUser = JSON.parse(sessionStorage.getItem('userDetails')!);
   const auth = inject(AuthService);
   if (!auth.isAuthenticated()) {
     auth.redirectToLogin();
@@ -11,10 +10,6 @@ export const authGuard: CanActivateFn = (route, state) => {
   }
   if (!auth.isUserActivated()) {
     auth.redirectToChangePassword();
-    return false;
-  }
-  if (route.data != null && route.data['projectTypes'] != null && !route.data['projectTypes'].includes(loggedInUser.ProjectType)) {
-    auth.redirectToMain();
     return false;
   }
   return true;
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -5,11 +5,9 @@ import { Router } from '@angular/router';
   providedIn: 'root'
 })
 export class AuthService {
-  constructor(private router: Router) {
-    
-  }
 
-  
+  constructor(private router: Router) { }
+
   isAuthenticated() {
     const userDetails = sessionStorage.getItem('userDetails');
     if (userDetails == null || userDetails == '') {
@@ -30,12 +28,8 @@ export class AuthService {
   redirectToChangePassword() {
     this.router.navigate(['/changepassword']);
   }
-  redirectToMain() {
-    this.router.navigate(['/dashboard/main']);
-  }
   logout() {
     sessionStorage.clear();
     this.router.navigate(['/login']);
   }
-  
 }
